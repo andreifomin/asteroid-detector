ded39152f510479a30009488a920b7a28552c996
Andrei Fomin <aifomin888@gmail.com>
2024-05-06 12:52:57 -0500

    Refactor ".then/.catch" into "async/await". Fix returned error data

--- src/callbacks/getFilterData.js
+++ src/callbacks/getFilterData.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 require("dotenv").config();
 
-function getFilterData(req, res) {
+async function getFilterData(req, res) {
   const { dateStart, dateEnd, withinValue, withinUnits } = req.body;
 
   // Modifying incoming data format to meet the task requirements and avoid hardcoding.
@@ -19,38 +19,42 @@ function getFilterData(req, res) {
   const apiUrl = "https://api.nasa.gov/neo/rest/v1/feed";
   const requestUrl = `${apiUrl}?start_date=${requestData.dateStart}&end_date=${requestData.dateEnd}&api_key=${apiKey}`;
 
-  axios.get(requestUrl)
-    .then(response => {
-      // Getting an array with all asteroid data from the response.
-      const allAsteroidRawData = Object.values(response.data.near_earth_objects).reduce((total, amount) => {
-        return total.concat(amount);
-      }, []);
+  try {
+    const response = await axios.get(requestUrl);
 
-      // Filtering asteroid data to name and distance (both distance units).
-      const allAsteroidNamesAndDistance = allAsteroidRawData.map(elem => {
-        return {
-          name: elem.name,  // Names are not changed.
-          distance: {
-            kilometers: Number(elem.close_approach_data[0].miss_distance.kilometers),
-            miles: Number(elem.close_approach_data[0].miss_distance.miles),
-          },
-        };
-      });
+    // Getting an array with all asteroid data from the response.
+    const allAsteroidRawData = Object.values(response.data.near_earth_objects).reduce((total, amount) => {
+      return total.concat(amount);
+    }, []);
 
-      // Filtering asteroids - only those within given distance are left.
-      const asteroidNames = allAsteroidNamesAndDistance
-        .filter(elem => elem.distance[requestData.within.units] <= requestData.within.value)
-        .map(elem => elem.name);
-
-      const responseData = {
-        asteroids: [... new Set(asteroidNames)],
+    // Filtering asteroid data to name and distance (both distance units).
+    const allAsteroidNamesAndDistance = allAsteroidRawData.map(elem => {
+      return {
+        name: elem.name,  // Names are not changed.
+        distance: {
+          kilometers: Number(elem.close_approach_data[0].miss_distance.kilometers),
+          miles: Number(elem.close_approach_data[0].miss_distance.miles),
+        },
       };
-
-      res.json(responseData);
-    })
-    .catch(error => {
-      console.error("Error: ", error.message);
     });
+
+    // Filtering asteroids - only those within given distance are left.
+    const asteroidNames = allAsteroidNamesAndDistance
+      .filter(elem => elem.distance[requestData.within.units] <= requestData.within.value)
+      .map(elem => elem.name);
+
+    const responseData = {
+      asteroids: [...new Set(asteroidNames)],
+    };
+
+    res.json(responseData);
+  } catch {
+    const errorData = {
+      error: true,
+    };
+
+    res.json(errorData);
+  }
 }
 
 module.exports = getFilterData;
